553333fd75b477d957cfea77446dc13b20d4dbbf
GeovaneMartinszz <162984386+GeovaneMartinszz@users.noreply.github.com>
2024-04-24 08:00:34 -0300

    Add files via upload

---
+++ dicionario.py
@@ -0,0 +1,25 @@
+ListaNomes = []
+
+
+ListaNomes.append("Edson")
+ListaNomes.append("Tamires")
+ListaNomes.append("Sofia")
+
+print(ListaNomes)
+print(ListaNomes[1])
+
+for nome in ListaNomes:
+    print(nome)
+
+PrimerioDicionario = {}
+
+PrimerioDicionario['nome'] = "Edson"
+PrimerioDicionario['tipo'] = "Professor"
+PrimerioDicionario['numero'] = 9
+print(PrimerioDicionario)
+print(PrimerioDicionario["tipo"])
+
+
+# Anatomia
+# Lista[indice] = Valor
+# dicionario[chave] = Valor
